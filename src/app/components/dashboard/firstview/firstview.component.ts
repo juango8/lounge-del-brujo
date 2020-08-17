c29e2e1063fb941a7254531b3642b1a86bd69cfd
unknown <eder.cornejo.diaz@ucsp.edu.pe>
2020-08-17 01:43:59 -0500

    real time app

--- src/app/components/dashboard/firstview/firstview.component.ts
+++ src/app/components/dashboard/firstview/firstview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../../../services/dashboard.service';
 import { HttpClient } from '@angular/common/http';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-firstview',
@@ -11,12 +12,14 @@ export class FirstviewComponent implements OnInit {
   public ordersEfectivo:any;
   public ordersYape:any;
   public ordersStripe:any;
+  public intervalo = interval(10000);
   paidState:boolean = false;
   confirmedState:boolean = false;
   test: any[] = [];
   paid:any;
   confirmed:any;
   myId:number;
+  subscription: Subscription;
 
   constructor(
     private _dashboardService:DashboardService,
@@ -56,8 +59,6 @@ export class FirstviewComponent implements OnInit {
     this.paid = update;
     this.myId = id;
     this.updatePaid(this.myId);
-    console.log(this.myId);
-    console.log(this.paid);
   }
 
   setconfirmedState(value:boolean, id:number){
@@ -68,7 +69,19 @@ export class FirstviewComponent implements OnInit {
     this.confirmed = update;
     this.myId = id;
     this.updateConfirmed(this.myId);
-    console.log(this.myId);
-    console.log(this.confirmed);
+  }
+
+  subscribeDataInterval(){
+      const intervalo = interval(2000)
+      this.subscription = intervalo.subscribe(n =>this._dashboardService.getDasboard().subscribe((data: any) => {
+        this.test = data.payment_methods;
+        this.ordersEfectivo = data.payment_methods[0];
+        this.ordersYape = data.payment_methods[1];
+        this.ordersStripe = data.payment_methods[2];
+      }));
+  }
+
+  unsubscribeDataInterval(){
+    this.subscription.unsubscribe();
   }
 }
