c11902fad0dd61c966cd2684b84de79df9aef6bf
juango <jgonzalofh2898@gmail.com>
2020-08-26 09:58:31 -0500

    dynamic schedule find-us.component

--- src/app/components/find-us/find-us.component.ts
+++ src/app/components/find-us/find-us.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {ScheduleService} from '../../services/schedule.service';
 
 @Component({
   selector: 'app-find-us',
   templateUrl: './find-us.component.html',
   styleUrls: ['./find-us.component.css']
 })
-export class FindUsComponent implements OnInit {
+export class FindUsComponent {
+    stores: Store[];
+    endDay: string;
+    endTime: string;
+    id: number;
+    name: string;
+    startDay: string;
+    startTime: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: ScheduleService) {
+      this.store.getProductsCategory().subscribe((data: any) => {
+          this.stores = data;
+          console.log(data);
+      });
   }
+}
 
+interface Store {
+    end_day: string;
+    end_time: string;
+    id: number;
+    name: string;
+    start_day: string;
+    start_time: string;
 }
